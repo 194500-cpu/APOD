d1f6768e8ac1b734321b780d2fe083a32d5c49c6
Colgation <194500@cloud.rangitoto.school.nz>
2026-09-15 20:19:35 +1200

    Cool basic planet form

--- src/main.js
+++ src/main.js
@@ -2,3 +2,83 @@ import '../style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 console.log(THREE);
+
+
+var planets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune"
+]
+
+
+var currentplanet = 0;
+
+function cyclePlanets() {
+  console.log(currentplanet);
+  currentplanet++;
+  if (currentplanet >= 8) {
+    currentplanet = 0;
+  }
+
+}
+document.getElementById("cyclebutton")
+document.addEventListener("click", cyclePlanets);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  alpha: true
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.getElementById("app").appendChild(renderer.domElement);
+
+const geometry = new THREE.SphereGeometry(1, 64, 64);
+
+const material = new THREE.MeshStandardMaterial({
+  color: 0x4477ff
+});
+
+const planet = new THREE.Mesh(geometry, material);
+
+scene.add(planet);
+
+const light = new THREE.DirectionalLight(0xffffff, 3);
+
+light.position.set(5, 3, 5);
+
+scene.add(light);
+
+scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+controls.enableDamping = true;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  planet.rotation.y += 0.005;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
