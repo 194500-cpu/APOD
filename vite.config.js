955a84ea93f5c5bc35474d5e2438ddb19f71dcfb
Colgation <194500@cloud.rangitoto.school.nz>
2026-09-16 20:44:20 +1200

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        planet: resolve(__dirname, 'planet.html')
+        planet: resolve(__dirname, 'planets.html')
       }
     }
   }
